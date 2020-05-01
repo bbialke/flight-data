dfbcdf3020741026431d1c31201ae652726485e9
bbialke <bradbialke05@gmail.com>
2020-04-30 21:18:00 -0700

    Added lookup by flight number

---
+++ index.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+exports.byNumber = function(API_TOKEN, FLIGHT_NUMBER) {
+  //Parameters for the axios request
+  const params = {
+    access_key: API_TOKEN,
+    flight_number: FLIGHT_NUMBER
+  }
+  //Make the request
+  console.log("Making request...");
+  axios.get('http://api.aviationstack.com/v1/flights', {params})
+  .then(function (response) {
+    // handle success
+    const apiResponse = response.data;
+    console.log(apiResponse);
+    apiResponse['data'].forEach(flight => {
+      if (flight['flight_status'] == 'active') {
+        console.log(`${flight['airline']['name']} flight ${flight['flight']['iata']}`,
+        `from ${flight['departure']['airport']} (${flight['departure']['iata']})`,
+        `to ${flight['arrival']['airport']} (${flight['arrival']['iata']}) is in the air.`);
+      }
+    });
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+    console.log(`Done.`)
+  });
+}
