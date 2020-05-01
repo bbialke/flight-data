06bcbc3e0279dd3f8dbd2ba4804506f4aa256e35
bbialke <bradbialke05@gmail.com>
2020-05-01 13:42:02 -0700

    Finished first unit test

--- functions/byNumber.js
+++ functions/byNumber.js
@@ -1,4 +1,5 @@
 const axios = require('axios').default;
+let err;
 
 async function byNumber(API_TOKEN, FLIGHT_NUMBER) {
   //Parameters for the axios request
@@ -11,8 +12,9 @@ async function byNumber(API_TOKEN, FLIGHT_NUMBER) {
   axios.get('http://api.aviationstack.com/v1/flights', {params})
   .then(response => {
     // handle success
-    console.log(response);
+    //console.log(response); Do not log full response
     const apiResponse = response.data;
+    //Log just the data section of the response
     console.log(apiResponse);
     apiResponse['data'].forEach(flight => {
       if (flight['flight_status'] == 'active') {
@@ -26,15 +28,18 @@ async function byNumber(API_TOKEN, FLIGHT_NUMBER) {
     if (error.response) {
       // The request was made and the server responded with a
       // status code that falls out of the range of 2xx
-      console.log(error.response.status);
-      // return Promise.reject(new Error(error.response.status))
+      let err = error.response.status;
+      console.log(`${err} error occured.`);
     }
   })
   .finally(function () {
     // always executed
     console.log(`Done.`)
   });
-    return Promise.reject(new Error('Error'))
+  //Reject async promise if an error occured
+  if(err){
+    return Promise.reject(new Error(err))
+  }
 }
 
 module.exports = byNumber;
