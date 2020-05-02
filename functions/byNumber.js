f7b8f7a0cfffe39fbfd57862b11f3dcf3cb51368
bbialke <bradbialke05@gmail.com>
2020-05-01 23:02:32 -0700

    Moved to callback handling for response

--- functions/byNumber.js
+++ functions/byNumber.js
@@ -1,7 +1,7 @@
 const axios = require('axios').default;
 let err = 'none';
 
-async function byNumber(API_TOKEN, FLIGHT_NUMBER, LIMIT) {
+async function byNumber(API_TOKEN, FLIGHT_NUMBER, callback, LIMIT) {
   //Parameters for the axios request
   const params = {
     access_key: API_TOKEN,
@@ -9,7 +9,7 @@ async function byNumber(API_TOKEN, FLIGHT_NUMBER, LIMIT) {
     limit: LIMIT
   }
   //Make the request
-  console.log("Making request...");
+  //console.log("Making request...");
   await axios.get('http://api.aviationstack.com/v1/flights', {params})
   .then(response => {
     // handle success
@@ -17,22 +17,22 @@ async function byNumber(API_TOKEN, FLIGHT_NUMBER, LIMIT) {
     const apiResponse = response.data['data'];
     const responseCount = response.data['pagination'].count;
     //Log the response and response count
-    console.log(apiResponse);
-    console.log(`Recieved ${responseCount} responses.`);
+    //console.log(apiResponse);
+    //console.log(`Recieved ${responseCount} responses.`);
     //Return the response data array
-    return apiResponse;
+    callback(apiResponse);
   })
   .catch(error => {
     if (error.response) {
       // The request was made and the server responded with a
       // status code that falls out of the range of 2xx
       err = error.response.status;
-      console.log(`${err} error occured.`);
+      //console.log(`${err} error occured.`);
     }
   })
   .finally(function () {
     // always executed
-    console.log(`Done.`)
+    //console.log(`Done.`)
   });
   //Reject async promise if an error occured
   if(err !== 'none'){
