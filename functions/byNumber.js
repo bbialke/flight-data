3468202195dc8be345bb9aa7545e9dc11a89d369
bbialke <bradbialke05@gmail.com>
2020-05-01 21:09:09 -0700

    Rewrote error handling

--- functions/byNumber.js
+++ functions/byNumber.js
@@ -1,34 +1,32 @@
 const axios = require('axios').default;
-let err;
+let err = 'none';
 
-async function byNumber(API_TOKEN, FLIGHT_NUMBER) {
+async function byNumber(API_TOKEN, FLIGHT_NUMBER, LIMIT) {
   //Parameters for the axios request
   const params = {
     access_key: API_TOKEN,
-    flight_number: FLIGHT_NUMBER
+    flight_number: FLIGHT_NUMBER,
+    limit: LIMIT
   }
   //Make the request
   console.log("Making request...");
-  axios.get('http://api.aviationstack.com/v1/flights', {params})
+  await axios.get('http://api.aviationstack.com/v1/flights', {params})
   .then(response => {
     // handle success
-    //console.log(response); Do not log full response
-    const apiResponse = response.data;
-    //Log just the data section of the response
+    //Get the data array from the response, and the number of results returned.
+    const apiResponse = response.data['data'];
+    const responseCount = response.data['pagination'].count;
+    //Log the response and response count
     console.log(apiResponse);
-    apiResponse['data'].forEach(flight => {
-      if (flight['flight_status'] == 'active') {
-        console.log(`${flight['airline']['name']} flight ${flight['flight']['iata']}`,
-        `from ${flight['departure']['airport']} (${flight['departure']['iata']})`,
-        `to ${flight['arrival']['airport']} (${flight['arrival']['iata']}) is in the air.`);
-      }
-    });
+    console.log(`Recieved ${responseCount} responses.`);
+    //Return the response data array
+    return apiResponse;
   })
   .catch(error => {
     if (error.response) {
       // The request was made and the server responded with a
       // status code that falls out of the range of 2xx
-      let err = error.response.status;
+      err = error.response.status;
       console.log(`${err} error occured.`);
     }
   })
@@ -37,8 +35,8 @@ async function byNumber(API_TOKEN, FLIGHT_NUMBER) {
     console.log(`Done.`)
   });
   //Reject async promise if an error occured
-  if(err){
-    return Promise.reject(new Error(err))
+  if(err !== 'none'){
+    return Promise.reject(new Error(err));
   }
 }
 
--- test/byNumber.test.js
+++ test/byNumber.test.js
@@ -2,15 +2,19 @@ const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const byNumber = require('../functions/byNumber');
 const assert = require('assert');
+const apiToken = require('../config.json').apiToken;
 
 chai.use(chaiAsPromised);
 const { expect } = chai;
 
 describe('testing lookup by flight number', () => {
+  it('should be rejected with error because API_TOKEN was not supplied', () => {
+    return expect(byNumber('','6514')).to.be.eventually.rejectedWith(Error);
+  });
   it('should be rejected with error because API_TOKEN is invalid', () => {
     return expect(byNumber('1234567890','6514')).to.be.eventually.rejectedWith(Error);
   });
-  it('should return 9', () => {
-    assert.equal(3 * 3, 9);
+  it('should be rejected with error because FLIGHT_NUMBER is invalid', () => {
+    return expect(byNumber(apiToken,'qqqq')).to.be.eventually.rejectedWith(Error);
   });
 });
