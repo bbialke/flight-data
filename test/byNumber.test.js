ba67eb29dca14d7cb304058c5019fc089fd641ff
bbialke <bradbialke05@gmail.com>
2020-05-02 22:01:37 -0700

    Removed .eventually from tests

--- test/byNumber.test.js
+++ test/byNumber.test.js
@@ -9,12 +9,12 @@ const { expect } = chai;
 
 describe('testing lookup by flight number', () => {
   it('should be rejected with error because API_TOKEN was not supplied', () => {
-    return expect(byNumber('','6514')).to.be.eventually.rejectedWith(Error);
+    return expect(byNumber('','6514')).to.be.rejectedWith(Error);
   });
   it('should be rejected with error because API_TOKEN is invalid', () => {
-    return expect(byNumber('1234567890','6514')).to.be.eventually.rejectedWith(Error);
+    return expect(byNumber('1234567890','6514')).to.be.rejectedWith(Error);
   });
   it('should be rejected with error because FLIGHT_NUMBER is invalid', () => {
-    return expect(byNumber(apiToken,'qqqq')).to.be.eventually.rejectedWith(Error);
+    return expect(byNumber(apiToken,'qqqq',function(response){})).to.be.rejectedWith(Error);
   });
 });
