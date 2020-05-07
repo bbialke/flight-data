38a7bae3d244075bb10c38c994b4be6ebec270e1
bbialke <bradbialke05@gmail.com>
2020-05-06 18:24:00 -0700

    Added airports function, sample tests and references

---
+++ functions/airports.js
@@ -0,0 +1,32 @@
+const fetchData = require('../util/fetchData');
+const formatOptions = require('../util/formatOptions');
+
+function mergeData(fetchedData) {
+  const mergedData = { count: 0, data: [] };
+  fetchedData.forEach((dataObject) => {
+    mergedData.count += dataObject.pagination['count'];
+    mergedData.data = mergedData.data.concat(dataObject.data);
+  });
+  return mergedData;
+}
+
+async function flights({
+  API_TOKEN, options
+}) {
+  const URL = 'http://api.aviationstack.com/v1/airports';
+  if (API_TOKEN === undefined) {
+    throw new Error('No API_TOKEN provided, add your API_TOKEN from http://aviationstack.com/ as an argument');
+  }
+  const optionQuery = formatOptions(options);
+  try {
+    const data = [];
+    data.push(fetchData(URL, API_TOKEN, optionQuery));
+    const fetchedData = await Promise.all(data);
+    return mergeData(fetchedData);
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+module.exports = flights;
---
+++ test/airports.test.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const flights = require('../functions/flights');
+const assert = require('assert');
+const apiToken = require('../config.json').apiToken;
+
+chai.use(chaiAsPromised);
+const { expect } = chai;
+
+describe('testing errors in parameters', () => {
+  it('should be rejected with error because API_TOKEN was not supplied', () => {
+    return expect(flights({API_TOKEN: '', options: {limit: 1}})).to.be.rejectedWith(Error);
+  });
+  it('should be rejected with error because API_TOKEN is invalid', () => {
+    return expect(flights({API_TOKEN: 'thisisntvalid', options: {limit: 1}})).to.be.rejectedWith(Error);
+  });
+  it('should be rejected with error because FLIGHT_DATE is in an incorrect format', () => {
+    return expect(flights({API_TOKEN: apiToken, options: {limit: 1, flight_date: '05-25-2020'}})).to.be.rejectedWith(Error);
+  });
+  it('should be rejected with error because LIMIT is >100', () => {
+    return expect(flights({API_TOKEN: apiToken, options: {limit: 1000}})).to.be.rejectedWith(Error);
+  });
+});
+describe('testing successful queries', () => {
+  it('should return data of one flight without error', async () => {
+    const data = await flights({API_TOKEN: apiToken, options: {limit: 1, flight_number: '9333', arr_icao: 'ZSFZ'}});
+    expect(data.count).to.be.equals(1);
+    expect(data['data']).to.be.length(1);
+  });
+});
